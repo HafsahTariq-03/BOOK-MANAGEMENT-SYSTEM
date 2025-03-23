fe5682dc1b751cef5e9814c3e2e5af6608929ad2
HafsahTariq-03 <175113561+HafsahTariq-03@users.noreply.github.com>
2025-03-23 06:49:55 +0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL =
-  'mongodb+srv://hafsahphone3:eE3yvGZ2Ax_qH4y@book-store-mern.qmpya.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-Mern';
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
