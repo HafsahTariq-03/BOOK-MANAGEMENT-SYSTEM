0bd58a75fb9ba1c7ef9b95c1e66dd4065011d372
HafsahTariq-03 <175113561+HafsahTariq-03@users.noreply.github.com>
2025-03-24 07:04:58 +0500

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 5555;
 
-export const mongoDBURL = process.env.MONGODB_URI || 
+export const mongoDBURL = 
   'mongodb+srv://hafsahphone3:eE3yvGZ2Ax_qH4y@book-store-mern.qmpya.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-Mern';
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
