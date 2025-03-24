f83e1d061648d7e7427ec413bc20661925765da7
HafsahTariq-03 <175113561+HafsahTariq-03@users.noreply.github.com>
2025-03-24 12:33:54 +0500

    Update DeleteBook.jsx

--- Frontend/src/pages/DeleteBook.jsx
+++ Frontend/src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`http://34.226.249.134:5555/books/${id}`)
+      .delete(`http://44.201.103.207:5555/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
