4d5e3867b7e99dda4e6f6a682002936f89a865b1
HafsahTariq-03 <175113561+HafsahTariq-03@users.noreply.github.com>
2025-03-24 07:59:37 +0500

    Update ShowBook.jsx

--- Frontend/src/pages/ShowBook.jsx
+++ Frontend/src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books/${id}`)
+      .get(`http://34.226.249.134:5555/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
