7cc2c0923fd4e43793cddbe5d756b499805af0f0
HafsahTariq-03 <175113561+HafsahTariq-03@users.noreply.github.com>
2025-03-24 15:21:59 +0500

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = process.env.PORT || 5555;
+export const PORT = process.env.PORT || 6000;
 
 export const mongoDBURL = 
   'mongodb+srv://hafsahphone3:eE3yvGZ2Ax_qH4y@book-store-mern.qmpya.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-Mern';
